ab1f32d3c4b95ef34f4b29571c991c5071331969
LiteralPandamonium <mayen.ps@gmail.com>
2017-04-06 12:20:28 -0500

    Update from 4/5

--- CAT_ATTACK_MAIN.java
+++ CAT_ATTACK_MAIN.java
@@ -7,10 +7,15 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.TextArea;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+import com.vaadin.ui.themes.ValoTheme;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -27,19 +32,67 @@ public class CAT_ATTACK_MAIN extends UI {
         final VerticalLayout mainVL = new VerticalLayout();
         final HorizontalLayout hl1 = new HorizontalLayout();
         final HorizontalLayout hl2 = new HorizontalLayout();
+        final HorizontalLayout hl3 = new HorizontalLayout();
         
+        // Stuff going in the first horizontal layout. 
         final Label appName = new Label("CATattack");
-        hl1.addComponent(appName);
+        appName.setStyleName(ValoTheme.LABEL_H2);
         
-               final Panel column1 = new Panel();
+        final Label user = new Label("UserName");
+        user.setWidth("900px");
+        user.addStyleName("v-align-right");
+        user.addStyleName(ValoTheme.LABEL_H2);
+        
+        final Button acc = new Button("Account");
+        acc.setHeight("45px");
+        
+        final Button logOut = new Button("Log Out");
+        logOut.setHeight("45px");
+        
+        
+        //stuff for second horizontal layout 
+        
+        final Label project = new Label("PROJECT NAME ");
+        project.addStyleName(ValoTheme.LABEL_H3);
+        
+        final Label arrow = new Label(" > ");
+        arrow.addStyleName(ValoTheme.LABEL_H3);
+        
+        final Label sprintName = new Label(" SPRINT NAME");
+        sprintName.addStyleName(ValoTheme.LABEL_H3);
+        sprintName.setWidth("725px");
+        
+        final Button bDButton = new Button("Burndown Chart");
+        bDButton.setHeight("45px");
+        bDButton.addClickListener(event -> {
+        	
+        	Window subWindow = new Window();
+        	VerticalLayout subContent = new VerticalLayout();
+            subWindow.setContent(subContent);
+            subWindow.setHeight("600px");
+            subWindow.setWidth("700px");
+            // Put some components in it
+            subContent.addComponent(new Label("Burndown Chart"));
+
+            // Center it in the browser window
+            subWindow.center();
+
+            // Open it in the UI
+            addWindow(subWindow);
+        	
+        });
+        //stuff for third horizontal layout
+        final Panel column1 = new Panel();
         final Panel column2 = new Panel();
         final Panel column3 = new Panel();
         final Panel column4 = new Panel();
         
-        final Label header1 = new Label("Story");
+        final Label header1 = new Label("STORY");
         final Label header2 = new Label("TO DO");
         final Label header3 = new Label("DOING");
         final Label header4 = new Label("DONE");
+        
+        final Button plusBtn = new Button("+");
         
         column1.setWidth("300px");
         column2.setWidth("300px");
@@ -51,9 +104,18 @@ public class CAT_ATTACK_MAIN extends UI {
         column3.setContent(header3);
         column4.setContent(header4);
         
-        hl2.addComponents(column1, column2, column3, column4);
+        column1.setStyleName("v-align-center");
+       	column2.setStyleName("v-align-center");
+        column3.setStyleName("v-align-center");
+        column4.setStyleName("v-align-center");
         
-        mainVL.addComponents(hl1, hl2);
+        
+        
+        hl1.addComponents(appName, user, acc, logOut);
+        hl2.addComponents(project, arrow, sprintName, bDButton);
+        hl3.addComponents(plusBtn, column1, column2, column3, column4);
+        
+        mainVL.addComponents(hl1, hl2, hl3);
         
         setContent(mainVL);
     }
