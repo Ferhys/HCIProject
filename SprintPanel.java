4041374f85ddabb86ad66045c04dbfe05a19dd39
stephsparks <sparks@ou.edu>
2017-04-25 13:19:05 -0500

    Update SprintPanel.java

--- SprintPanel.java
+++ SprintPanel.java
@@ -1,38 +1,55 @@
 package projectNav;
 
+import java.util.ArrayList;
+
+import com.vaadin.navigator.Navigator;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 
+import kanban.KanbanView;
 import model.Project;
 import model.Sprint;
 
 public class SprintPanel extends VerticalLayout {
+
+	final ArrayList<Sprint> sprintList;
 	
-	public SprintPanel(Project project) {
+	public SprintPanel(Project project, Navigator nav, KanbanView nextView) {
+		this.sprintList = project.getSprintList();
 		
-		for (Sprint sprint:project.getSprintList()) {
+		for (Sprint sprint:sprintList) {
+			
 			Panel panel = new Panel();
 			HorizontalLayout hl = new HorizontalLayout();
-			Label name = new Label(sprint.getName());
+			Label name = new Label("Sprint " + i + ": " + sprint.getName());
 			name.setStyleName(ValoTheme.LABEL_BOLD);
 			Label startDate = new Label("Start date: " + sprint.getStartDate().toString());
-		//	Label endDate =   new Label("End date:   " + sprint.getEndDate().toString());
+			//	Label endDate =   new Label("End date:   " + sprint.getEndDate().toString());
 			startDate.setStyleName(ValoTheme.LABEL_LIGHT);
-			hl.addComponents(name);
+			
+			hl.addComponents(name, startDate);
 			hl.setSpacing(true);
 			panel.setContent(hl);
+			
+			
 			panel.addStyleName(ValoTheme.PANEL_WELL);
 			addComponent(panel);
+			
+			panel.addClickListener(e -> {
+				nextView.setSprint(sprint);
+				nav.navigateTo(nextView.VIEW_NAME);
+			});
 		}
-		
+
 		setSpacing(true);
 		setMargin(false);
-		 
-		 
-		
+
+
+
 	}
-	
-	
+
+
+}
