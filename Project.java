358cf8a843ad1358a6a78ab1978a3cb28ed10cb0
LiteralPandamonium <mayen.ps@gmail.com>
2017-04-25 12:57:24 -0500

    Update Project.java

--- Project.java
+++ Project.java
@@ -8,6 +8,8 @@ public class Project {
 	public String name;
 	private String description;
 	public ArrayList<Sprint> sprintList;
+	//TODO: add User class
+	//public ArrayList<User> userList;
 	private int status;
 	private LocalDate startDate;
 	
@@ -55,8 +57,15 @@ public class Project {
 		this.startDate = localDate;
 	}
 	
-	public LocalDate getStartDate() {
+	public LocalDate getStartDate(){
 		return startDate;
 	}
 	
+	public ArrayList<Sprint> getSprintList(){
+		return sprintList;
+	}
+	
+	public Sprint getSprint(int index){
+		return sprintList.get(index);
+	}
 }
