40b6857a2c49763be6bac64a5780a7c57d1b2623
LiteralPandamonium <mayen.ps@gmail.com>
2017-04-25 20:59:52 -0500

    Add files via upload

--- AddStory.java
+++ AddStory.java
@@ -1,69 +1,86 @@
 package kanban;
 
+import java.time.LocalDate;
+
+import com.vaadin.data.Binder;
+import com.vaadin.data.ValidationException;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.DateField;
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
-public class AddStory {
+import model.Sprint;
+import model.Story;
+
+public class AddStory extends Window {
 	
+	final TextField storyName;
+	final TextField description;
+	final DateField startDate;
+	final DateField endDate;
 	
-	static Window newStory(GridLayout GL) {
+	public AddStory() {
 		//Add a new story to the label. 
-     //	Story st = new Story();
-     	Panel panel = new Panel();
-     	panel.addClickListener(e->{
-     		//If you click on the story, should cause the storie's tasks to show. 
-     		
-     	});
-     	panel.setWidth("300px");
-     	
-      	Window sW = new Window();
-    	VerticalLayout subContent = new VerticalLayout();
+    	VerticalLayout main = new VerticalLayout();
     	HorizontalLayout btnContent = new HorizontalLayout();
-    	subContent.setStyleName("v-align-center");
+    	main.setStyleName("v-align-center");
     	btnContent.setStyleName("v-align-center");
-        sW.setContent(subContent);
-        sW.setHeight("200px");
-        sW.setWidth("400px");
-        TextField nameInput = new TextField("Enter Story Name");
-        nameInput.focus();
+        storyName = new TextField("Story Name");
+        description = new TextField("Story description: ");
+        startDate = new DateField("Start Date");
+        endDate = new DateField("End Date");
+        storyName.focus();
         Button addBtn = new Button("Add");
-        panel.setStyleName("valo-animate-in-fade");
+    
         addBtn.addClickListener(e -> {
-        	//st.setName(nameInput.getValue());
-        	panel.setStyleName("v-align-center");
-        	Label lbl = new Label(nameInput.getValue());
-        	HorizontalLayout stLayout = new HorizontalLayout();
-        	Button addTask = new Button("+");
-        	addTask.setHeight("25px");
-        	stLayout.addComponents(addTask, lbl);
-        	panel.setContent(stLayout);
-        	GL.addComponent(panel,1,1);
-        	sW.close();
+        	close();
         });
+        
         Button cancelBtn = new Button("Cancel");
         cancelBtn.addClickListener(e -> {
-        	sW.close();
+        	close();
         });
         
         
         // Put some components in it
-        subContent.addComponents(new Label("New Story"), nameInput, btnContent);
         btnContent.addComponents(addBtn, cancelBtn);
+        main.addComponents(new Label("New Story"), storyName, startDate,endDate, btnContent);
+        setContent(main);
 
         // Center it in the browser window
-        sW.center();
-        
-        // Open it in the UI
-       	return sW;
-
-    	
+ 
 		
+	}
+	
+	public Story getStory(){
+		Story st = new Story();
+		
+		Binder<Story> binder = new Binder<>();
+		binder.bind(storyName, Story::getName, Story::setName);
+		binder.bind(description, Story::getDescription, Story::setDescription);
+		binder.bind(startDate, Story::getStartDate, Story::setStartDate);
+		binder.bind(endDate, Story::getEndDate, Story::setEndDate);
+		
+		try{
+			binder.writeBean(st);
+		}
+		catch(ValidationException e){
+			Notification.show("ERROR WRITING STORY");
+		}
+		return st;
+	}
+	
+	public void reset(){
+		storyName.setValue("");
+		description.setValue("");
+		startDate.setValue(LocalDate.now());
+		endDate.setValue(LocalDate.now());
 	}
 	
 	
