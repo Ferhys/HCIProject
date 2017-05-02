dc25a873aa26ad3959c1ceaebdcb4f99443a0e41
stephsparks <sparks@ou.edu>
2017-05-01 20:22:53 -0500

    Update StoryPanel.java

--- StoryPanel.java
+++ StoryPanel.java
@@ -1,35 +1,35 @@
 package kanban;
 
-import java.util.ArrayList;
-
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 
-import model.Sprint;
 import model.Story;
 
 public class StoryPanel extends Panel {
 
-	
-	
 	public StoryPanel(Story story) {
-			
-		
-	
-			HorizontalLayout hl = new HorizontalLayout();
-			Label name = new Label(story.getName());
-			name.setStyleName(ValoTheme.LABEL_BOLD);
-			Label startDate = new Label("Start date: " + story.getStartDate().toString());
-			//	Label endDate =   new Label("End date:   " + sprint.getEndDate().toString());
-			startDate.setStyleName(ValoTheme.LABEL_LIGHT);
-			
-			hl.addComponents(name, startDate);
-			hl.setSpacing(true);
-			setContent(hl);
 
-			this.addStyleName(ValoTheme.PANEL_WELL);
+		VerticalLayout vl = new VerticalLayout();
+		HorizontalLayout hl = new HorizontalLayout();
 		
+		//name and date
+		Label name = new Label(story.getName());
+		name.setStyleName(ValoTheme.LABEL_BOLD);
+		Label startDate = new Label("Start date: " + story.getStartDate().toString());
+		startDate.setStyleName(ValoTheme.LABEL_LIGHT);
+
+		Label description = new Label(story.getDescription());
+		description.setStyleName(ValoTheme.LABEL_SMALL);
+		
+		hl.addComponents(name, startDate);
+		vl.addComponents(hl, description);
+		hl.setSpacing(true);
+		setContent(hl);
+
+		this.addStyleName(ValoTheme.PANEL_WELL);
+
 	}
+}
