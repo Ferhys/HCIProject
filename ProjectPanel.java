ee6ed9224f24cd2b7dcaf86b91677add3e633594
stephsparks <sparks@ou.edu>
2017-05-01 19:29:45 -0500

    Update ProjectPanel.java

--- ProjectPanel.java
+++ ProjectPanel.java
@@ -1,5 +1,6 @@
 package projectNav;
 
+import com.vaadin.navigator.View;
 import com.vaadin.server.Page;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
@@ -14,10 +15,11 @@ import model.Project;
 
 public class ProjectPanel extends Panel {
 
-	public ProjectPanel(Project project) {
+	public ProjectPanel(Project project, double size, ProjectNavigatorView parentView) {
 		
 		VerticalLayout vl = new VerticalLayout();
 		HorizontalLayout hl = new HorizontalLayout();
+		hl.setWidth(size + "px");
 		
 		//Name and date
 		Label name = new Label(project.getName());
@@ -28,20 +30,18 @@ public class ProjectPanel extends Panel {
 		//edit or delete
 		Button del = new Button("del");
 		del.addClickListener(e -> {
-			Notification.show("deleting project.");
+			parentView.removeProject(project);
 		});
 		
 		//description
 		Label description = new Label(project.getDescription());
 		description.setStyleName(ValoTheme.LABEL_SMALL);
-		description.setWidth((Page.getCurrent().getBrowserWindowWidth() -100) / 2 + "px");
-	
+		description.setWidth(size + "px");
 
 		hl.addComponents(name, date, del);
-		//TODO: set width. that's why the button doesn't go to the top right.
+		hl.setSpacing(false);
 		hl.setComponentAlignment(del, Alignment.TOP_RIGHT);
 		vl.addComponents(hl, description);
-		vl.setSpacing(false);
 		
 		this.setContent(vl);
 		this.addStyleName(ValoTheme.PANEL_WELL);
